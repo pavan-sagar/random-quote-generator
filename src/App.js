9a7314ef16893d9482176cc659a5bb0a0ec78d2e
unknown <pavan.sagar@outlook.com>
2020-05-21 19:06:58 +0530

    Fixed height of quote container and added random background colors generation

--- src/App.js
+++ src/App.js
@@ -21,6 +21,8 @@ class App extends Component {
     }
 
       this.setRandomQuoteFromApi = this.setRandomQuoteFromApi.bind(this);
+      this.setRandomBackgroundColor = this.setRandomBackgroundColor.bind(this);
+      this.callToSetRandomQuoteAndColor = this.callToSetRandomQuoteAndColor.bind(this);
 
   }
 
@@ -46,6 +48,23 @@ class App extends Component {
 
   }
       
+  setRandomBackgroundColor(){
+
+    let hue = Math.random()*360;
+    let saturation = '70%' //(25 + 70 * Math.random())+'%'; //In the range of 25-95%
+    let lightness =  '50%' //(85 + 10 * Math.random())+'%'; //In the range of 85-95%
+
+    let colorString = 'hsl('+hue+','+saturation+','+lightness+')';
+    console.log(colorString)
+
+    this.setState({primaryColor:colorString})
+
+  }
+
+  callToSetRandomQuoteAndColor(){
+    this.setRandomQuoteFromApi();
+    this.setRandomBackgroundColor();
+  }
 
   render(){
     
@@ -54,23 +73,23 @@ class App extends Component {
     
     return (<div className="container-fluid vh-100" style={{backgroundColor:this.state.primaryColor}}>
 
-      <div className="bg-black rounded mx-auto text-center p-3 bg-white" style={{height:'25%',width:'30%',position:'relative',top:'37%'}}>
+      <div className="bg-black rounded mx-auto text-center p-3 bg-white" style={{height:'auto',width:'30%',position:'relative',top:'37%'}}>
       
         <p className="col-md-12">
           <i className="fa fa-quote-left" style={{fontSize:'1.5rem',color:this.state.primaryColor}} />
-          <span className='h2 p-4' style={{color:this.state.primaryColor}}>{this.state.randomQuote}</span>
+          <span className='h3 p-4' style={{color:this.state.primaryColor}}>{this.state.randomQuote}</span>
           <i className="fa fa-quote-right" style={{fontSize:'1.5rem',color:this.state.primaryColor}} />
 
         </p>
         
-        <p className='float-right pr-4 font-weight-normal' style={{position:'relative',bottom:'1rem',color:this.state.primaryColor}}>-{this.state.randomAuthor}</p>
+        <p className='float-right pr-4 pt-2 font-weight-normal' style={{position:'relative',bottom:'1rem',color:this.state.primaryColor}}>-{this.state.randomAuthor}</p>
         
         <br />
 
         <div className='btn-group w-100'>
           <button className="btn-dark rounded ml-2 p-0" style={{width:'8%',backgroundColor:this.state.primaryColor}}><i className="fa fa-twitter" style={{fontSize:'1.5rem',color:'white'}}></i></button>
           <button className="btn-dark rounded ml-2 p-0" style={{width:'8%',backgroundColor:this.state.primaryColor}}><i className="fa fa-tumblr" style={{fontSize:'1.5rem',color:'white'}}></i></button>
-          <button className="btn-dark float-right ml-auto px-2 py-1 mr-2 rounded" style={{width:'20%',backgroundColor:this.state.primaryColor}} onClick={this.setRandomQuoteFromApi}><span className="align-center">New Quote</span></button>
+          <button className="btn-dark float-right ml-auto px-2 py-1 mr-2 rounded" style={{width:'20%',backgroundColor:this.state.primaryColor}} onClick={this.callToSetRandomQuoteAndColor}><span className="align-center">New Quote</span></button>
         </div>
       </div>
       
